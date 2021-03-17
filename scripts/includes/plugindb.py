ca4f8f952303d314f88da19f72e7159e72c6d874
daviesnln <nigel.davies@LearningNexus.co.uk>
2021-03-17 07:58:18 +0000

    Added new plugin to the DB include

--- scripts/includes/plugindb.py
+++ scripts/includes/plugindb.py
@@ -94,10 +94,13 @@ new=[
 {"name":"auth_saml2","media":"git","repo":"ssh://git@github.com/catalyst/moodle-auth_saml2.git","branch":"2019022100","lmsversion":"10","path":"auth/saml2"},
 {"name":"auth_saml2","media":"git","repo":"ssh://git@github.com/catalyst/moodle-auth_saml2.git","branch":"2019022100","lmsversion":"9","path":"auth/saml2"},
 
-{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/14021/block_accessibility_moodle31_2017051700.zip","branch":"2017051700","lmsversion":"12","path":"block/"},
-{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/14021/block_accessibility_moodle31_2017051700.zip","branch":"2017051700","lmsversion":"11","path":"block/"},
-{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/14021/block_accessibility_moodle31_2017051700.zip","branch":"2017051700","lmsversion":"10","path":"block/"},
-{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/8385/block_accessibility_moodle29_2015011600.zip","branch":"2015011600","lmsversion":"9","path":"block/"}
+{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/14021/block_accessibility_moodle29_2015011600.zip","branch":"2015011600","lmsversion":"12","path":"block/"},
+{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/14021/block_accessibility_moodle29_2015011600.zip","branch":"2015011600","lmsversion":"11","path":"block/"},
+{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/14021/block_accessibility_moodle29_2015011600.zip","branch":"2015011600","lmsversion":"10","path":"block/"},
+{"name":"block_accessibility","media":"curl","repo":"https://moodle.org/plugins/download.php/8385/block_accessibility_moodle29_2015011600.zip","branch":"2015011600","lmsversion":"9","path":"block/"},
+{"name":"auth_oidc","media":"curl","repo":"https://moodle.org/plugins/download.php/23298/auth_oidc_moodle310_2020110900.zip","branch":"2020110900","lmsversion":"12","path":"auth/"},
+{"name":"auth_oidc","media":"curl","repo":"https://moodle.org/plugins/download.php/23298/auth_oidc_moodle310_2020110900.zip","branch":"2020110900","lmsversion":"11","path":"auth/"}
+
 ]
 
 
