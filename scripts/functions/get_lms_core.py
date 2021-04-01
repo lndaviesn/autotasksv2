cdc947d049ddd85351836050b03fea01feb13d51
daviesnln <nigel.davies@LearningNexus.co.uk>
2021-04-01 12:52:16 +0100

    Added an new function to get the core LMS from file or repo. Tidy the code as well to make it eaiser

---
+++ scripts/functions/get_lms_core.py
@@ -0,0 +1,22 @@
+import tarfile, os
+from git import Repo
+from git import Git
+import functions.globalfun as glfun
+
+
+def get_file(lmsfile,extratpath):
+    if lmsfile.endswith("tar.gz"):
+        tar = tarfile.open(lmsfile, "r:gz")
+    elif lmsfile.endswith("tar"):
+        tar = tarfile.open(lmsfile, "r:")
+    tar.extractall(extratpath)
+    tar.close()
+
+
+def get_git(giturl,base,repoloc):
+    extratpath = repoloc + "/"+ base
+    git_ssh_cmd = 'ssh -o StrictHostKeyChecking=no'
+    with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
+        gitrepo = Repo.clone_from(giturl, extratpath, branch=base)
+    print ("Removing git folder")
+    glfun.gitclear(extratpath)
